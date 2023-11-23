912a46c311ff3f89e7b59915608033367fa02f81
yue <2794127100@qq.com>
2023-11-23 21:42:47 +0800

    my

--- nuxt3-app/pages/store.vue
+++ nuxt3-app/pages/store.vue
@@ -1,12 +1,13 @@
 <template>
-    <div>
-      <h1>store页面</h1>
-      <NuxtLink to="/store/store1">store1</NuxtLink>
-      <NuxtLink to="/store/store2">store2</NuxtLink>
-      <NuxtPage />
-    </div>
-  </template>
+  <div>
+    <h1>store页面</h1>
+    <NuxtLink to="/store/store1">store1</NuxtLink>
+    <NuxtLink to="/store/store2">store2</NuxtLink>
+    <NuxtPage />
+  </div>
+</template>
   
   <script lang="ts" setup>
-
-  </script>
+const headers = useRequestHeaders(["cookie"]);
+console.log("headers", headers);
+</script>
